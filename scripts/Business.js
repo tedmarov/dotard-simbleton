9a4aa93d98010e1968957952a28e783e1b0afc13
ted <tedmarov@gmail.com>
2021-06-15 09:20:47 -0500

    Added changes to clients

--- scripts/Business.js
+++ scripts/Business.js
@@ -4,30 +4,36 @@
 
  export const Business = (bizObject) => {
      return `
-     <section class="business">
+     <section class=business
             
-            <h2 class="business__companyName">
-                "${bizObject.companyName}"
+            <h2 class=business__companyName>
+                ${bizObject.companyName}
             </h2>
-            <p class="business__addressFullStreet">
-                "${bizObject.addressFullStreet}"
+            <p class=business__addressFullStreet>
+                ${bizObject.addressFullStreet}
             </p>
-            <p class="business__addressCityStateZip">
-                "${bizObject.addressCity}, ${bizObject.addressStateCode} ${bizObject.addressZipCode}"
+            <p class=business__addressCityStateZip>
+                ${bizObject.addressCity}, ${bizObject.addressStateCode} ${bizObject.addressZipCode}
             </p>
      </section>
      `
  }
 
-        /*  <div class="business__phoneWork">
-                Phone: "${bizObject.phoneWork}"
-            </div>
-            <div class="business__orders">
-                Orders: "${bizObject.orders}"
-            </div>
-            <div class="business__companyName">
-                Agent: "${bizObject.purchasingAgent.nameLast} ${bizObject.purchasingAgent.nameFirst}"
-            </div>
-            <div class="business__companyIndustry">
-                Company Industry: "${bizObject.companyIndustry}"
-            </div> */
+export const purchAgentHTMLCreate=(clientArray)=>{
+    return `
+    <section class=clients
+    
+        <div class=client__company>
+            Name: ${clientArray.purchasingAgent.firstName}
+        </div>
+        <div class=client__streetAddress>
+            Street: ${clientArray.companyName}
+        </div>
+        <div class=client__City>
+            City: ${clientArray.phoneWork}
+        </div>
+        
+        <br>
+        </section>
+        `
+}
